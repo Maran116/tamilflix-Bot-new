ade79b9a60ec18a2e014b0d77a2e483a22b45707
Maran116 <122981482+Maran116@users.noreply.github.com>
2024-10-14 10:49:17 +0500

    Update info.py

--- info.py
+++ info.py
@@ -48,7 +48,7 @@ else:
 INDEX_CHANNELS = [int(index_channels) if index_channels.startswith("-") else index_channels for index_channels in environ.get('INDEX_CHANNELS', '-1002406330623').split()]
 if len(INDEX_CHANNELS) == 0:
     print('Info - INDEX_CHANNELS is empty')
-AUTH_CHANNEL = [int(auth_channels) for auth_channels in environ.get('AUTH_CHANNEL', '-1002281100265').split()]
+AUTH_CHANNEL = [int(auth_channels) for auth_channels in environ.get('AUTH_CHANNEL', '-1002023479054').split()]
 if len(AUTH_CHANNEL) == 0:
     print('Info - AUTH_CHANNEL is empty')
 LOG_CHANNEL = environ.get('LOG_CHANNEL', '-1002406330623') #bot log channel -1005293546253
@@ -83,7 +83,7 @@ TUTORIAL = environ.get("TUTORIAL", "https://t.me/Rkotz")
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/Rkbotz")
 
 # Bot settings
-DELETE_TIME = int(environ.get('DELETE_TIME', 10)) # Add time in seconds 
+DELETE_TIME = int(environ.get('DELETE_TIME', 3600)) # Add time in seconds 
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 MAX_BTN = int(environ.get('MAX_BTN', 10)) #don't change anything in Language 
 LANGUAGES = [language.lower() for language in environ.get('LANGUAGES', 'english hindi telugu tamil kannada malayalam').split()]
